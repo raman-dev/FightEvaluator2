d538ac9d8339c5d2a9d7ee676f0f92105fb68235
raman-dev <ramandeep.dev582@gmail.com>
2023-11-06 22:02:44 -0500

    switched to templates instead of using fetch for everything

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from datetime import date
+from datetime import datetime
 from enum import Enum
 import scraper
 
@@ -90,6 +91,7 @@ class Note(SQLModel,table=True):
     id: int = Field(primary_key=True)
     assessment_id: int = Field(foreign_key="assessment.id")
     data: str
+    timestamp: str = Field(default=datetime.now().isoformat())
 
 class Fighter(SQLModel,table=True):
     id: int = Field(primary_key=True)
@@ -100,7 +102,7 @@ class Fighter(SQLModel,table=True):
     height: Optional[str]
     date_of_birth: Optional[date]
     #an optional foreign key to the matchup table if a matchup is scheduled
-    matchup_id: Optional[int] = Field(default=None, foreign_key="matchup.id")
+    # matchup_id: Optional[int] = Field(default=None, foreign_key="matchup.id")
     # matchup: Optional[MatchUp] = Relationship(back_populates="fighters")
     #optional since a fighter may need to still be assessed
     assessment_id: Optional[int] = Field(default=None, foreign_key="assessment.id")
@@ -110,8 +112,26 @@ class Fighter(SQLModel,table=True):
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+def weightclassStr(weightclass: WeightClass):
+    return weightclass.value.__str__()
+
+def none2Null(value):
+    if value == None:
+        return "null"
+    else:
+        return value
+
+def fightAttribQualifier(value):
+    if value == None:
+        return "null"
+    return '`'+value.value.__str__() +'`'
+
 app.mount("/static", StaticFiles(directory=Path("static")), name="static")
 templates = Jinja2Templates(directory="templates")
+templates.env.filters['weightclassStr'] = weightclassStr
+templates.env.filters['none2Null'] = none2Null
+templates.env.filters['fightAttribQualifier'] = fightAttribQualifier
+
 
 @app.on_event("startup")
 def on_start():
@@ -147,12 +167,39 @@ async def index(request: Request):
     #and create matchup objects for each matchup in the events
     #or do i create an endpoint that scrapes the next event and returns it
     # return FileResponse("static/index.html")
-    return templates.TemplateResponse("index.html",{"request": request})
+    context = {"request":request}
+    eventData = next_event()
+    context['event'] = eventData['event']
+    context['matchups'] = eventData['matchups']
+    return templates.TemplateResponse("index.html",context)#context
 
 #assessmnent require fighter id since they cannot exist without a fighter
 @app.get("/assessment/{fighter_id}")
-async def index():
-    return FileResponse("static/assessment.html")
+async def index(request: Request,fighter_id: int):
+    # return FileResponse("static/assessment.html")
+    context = {"request":request}
+    # context['fighter'] = get_fighter(fighter_id)
+    with Session(engine) as session:
+        fighter = session.get(Fighter,fighter_id)
+        if not fighter:
+            raise HTTPException(status_code=404, detail="Fighter not found")
+        assessment = session.get(Assessment,fighter.assessment_id)
+        if not assessment:
+            assessment = Assessment()
+            fighter.assessment_id = assessment.id
+            session.add(assessment)
+            session.commit()
+            session.refresh(assessment)
+        #get all notes with assessment_id
+        notes = session.query(Note).filter(Note.assessment_id == assessment.id).order_by(Note.timestamp.desc()).all()
+        if not notes:
+            notes = []
+        context['fighter'] = fighter
+        context['assessment'] = assessment
+        context['notes'] = notes
+        # print(type(assessment.__fields__.keys())
+        # context['assessment_keys'] = dict(zip(assessment.__fields__.keys(),assessment.__fields__.keys()))
+    return templates.TemplateResponse("assessment.html",context)
 
 @app.get("/assessment/data/{assessment_id}")
 async def get_assessment(assessment_id: int):
@@ -180,8 +227,8 @@ async def index(fighter_a: int | None = None,fighter_b: int | None = None):
         print('matchup for fighters -> ',fighter_a,fighter_b)  
     return FileResponse("static/matchup.html")
 
-@app.get("/nextevent")
-async def next_event():
+# @app.get("/nextevent")
+def next_event():
     event = None
     matchups = []
     with Session(engine) as session:
@@ -225,7 +272,7 @@ async def create_note(noteIn: NoteIn):#,request: Request):
     #add note to database
     # body = await request.body()
     # print('\nRECEIVED FROM BROWSER\n',body,noteIn.data)
-    note = Note(assessment_id=noteIn.assessment_id,data=noteIn.data)
+    note = Note(assessment_id=noteIn.assessment_id,data=noteIn.data,timestamp=datetime.now().isoformat())
     with Session(engine) as session:
         session.add(note)
         session.commit()
@@ -245,7 +292,7 @@ async def delete_notes(note_id):
 
 
 @app.get("/fighters/{fighter_id}")
-async def get_fighter(fighter_id:int):
+def get_fighter(fighter_id:int):
     print('getting fighter with id -> ',fighter_id)
     with Session(engine) as session:
         fighter = session.get(Fighter,fighter_id)
