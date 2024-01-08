0cad80b93573a9ba6f5e75fac09977141e956e33
raman-dev <ramandeep.dev582@gmail.com>
2024-01-07 21:58:29 -0500

    likelihood selector almost done

--- fightevaluator2/fightEvaluator/static/fightEvaluator/js/outcomes.js
+++ fightevaluator2/fightEvaluator/static/fightEvaluator/js/outcomes.js
@@ -4,6 +4,23 @@
 document.querySelectorAll('.confidence-selector').forEach(item => {
     let showConfidenceListBtn = item.querySelector('.show-confidence-list-btn');
     let confidenceList = item.querySelector('.confidence-list');
+    let confidence = item.querySelector('.current-confidence .confidence');
+    //add click event listener to list items of confidenceList
+    confidenceList.querySelectorAll('li').forEach(li => {
+        li.addEventListener('click', (event) => {
+            let listItem = event.currentTarget;
+            if (listItem.classList.contains('active')) {
+                //if clicked list item is active, remove active class
+                return;
+            }
+            let currentConfidence = confidenceList.querySelector('.active');
+            currentConfidence.classList.remove('active');
+            listItem.classList.add('active');
+            confidence.textContent = listItem.textContent;
+            confidence.classList.remove(currentConfidence.classList[0]);
+            confidence.classList.add(listItem.classList[0]);    
+        });
+    });
     showConfidenceListBtn.addEventListener('click', (event) => {
         //check if confidenceList is expanded
         let isOpen = confidenceList.classList.contains('expanded');
