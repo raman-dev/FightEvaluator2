2179917a26d76e507418a7dd914517337af03d73
raman-dev <ramandeep.dev582@gmail.com>
2024-02-20 23:05:48 -0500

    meh

--- fightevaluator2/fightEvaluator/templatetags/custom_filters.py
+++ fightevaluator2/fightEvaluator/templatetags/custom_filters.py
@@ -1,6 +1,16 @@
 from django import template
 register = template.Library()
+from datetime import datetime
 
 @register.filter
 def rmscores(value: str):
     return value.replace('_',' ')
+
+@register.filter
+def toage(value: datetime.date):
+    if not value:
+        return ""
+    #convert datetime.date to age 
+    today = datetime.now().date()
+    age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+    return age
