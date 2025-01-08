9d7e966acbf153071a1f112c04b5c765272b3344
raman-dev <ramandeep.dev582@gmail.com>
2025-01-07 23:48:14 -0500

    meh

---
+++ fightevaluator2/fightEvaluator/static/fightEvaluator/js/server.js
@@ -0,0 +1,45 @@
+class Server {
+
+    static URLS = {
+      CREATE_NOTE : '/notes/create-note',
+      DELETE_NOTE : '/notes/delete-note/',
+      UPDATE_FIGHTER :'/fighters/update-fighter2/'
+    }
+
+    constructor(headers) {
+        this.headers = headers
+    }
+
+    async update_fighter(requestData,callback,fighterId){
+      fetch(Server.URLS.UPDATE_FIGHTER + `${fighterId}`,{
+        method:"PATCH",
+        headers:this.headers,
+        body: JSON.stringify(requestData)
+      }).then(response => response.json())
+      .then((data)=>{
+        callback(data);//
+      });
+    }
+
+    async send_note(requestData,callback){
+      fetch(Server.URLS.CREATE_NOTE,{
+        method:"POST",
+        headers:this.headers,
+        body: JSON.stringify(requestData)
+      }).then(response => response.json())
+      .then((data)=>{
+        callback(data);//
+      });
+    }
+
+    async remove_note(callback,noteId){
+      //need error checking, parameter validation
+      fetch(Server.URLS.DELETE_NOTE + `${noteId}`,{
+        method:"DELETE",
+        headers:this.headers
+      }).then(response => response.json())
+      .then((data)=>{
+        callback(data);//
+      });
+    }
+  }
