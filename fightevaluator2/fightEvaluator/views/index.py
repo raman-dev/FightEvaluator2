d5b9cead6e270a7787557e6c39e6c513a1a7d834
raman-dev <ramandeep.dev582@gmail.com>
2025-01-24 00:00:52 -0500

    meh

--- fightevaluator2/fightEvaluator/views/index.py
+++ fightevaluator2/fightEvaluator/views/index.py
@@ -163,45 +163,6 @@ def events(request):
         events_by_month[month_year].append(event)
     return render(request,"fightEvaluator/events.html",{'events':events,'events_by_month':events_by_month})
 
-# class EventListView(ListView):
-#     model = FightEvent
-#     context_object_name = 'events'
-
-
-def getFightEndMethod(rawIn):
-    raw_method = rawIn.lower()
-    if 'decision' in raw_method:
-        return FightOutcome.Outcomes.DECISION
-    if 'ko' in raw_method:
-        return FightOutcome.Outcomes.KO
-    if 'submission' in raw_method:
-        return FightOutcome.Outcomes.SUBMISSION
-    if 'no contest' in raw_method:
-        return FightOutcome.Outcomes.NO_CONTEST
-    if 'draw' in raw_method:
-        return FightOutcome.Outcomes.DRAW
-    
-    return FightOutcome.Outcomes.NA
-
-def getFightEndTimeAndRound(raw_time):
-    #parsing time 
-    #if all rounds are completed
-    # print(raw_time)
-    # time format -> 3/5 rounds, 15:00/25:00 Total
-    rounds = 0
-    time = ""
-    if re.match(r'\d Rounds, \d{2}:\d{2} Total',raw_time):
-        #get the rounds split on the comma
-        rounds = re.search(r'\d',raw_time).group(0)#first number will be rounds
-        time = re.search(r'\d{2}:\d{2}',raw_time).group(0)#second number will be time
-    else:
-    # time format -> 1:23 Round X of Y
-        time = re.search(r'\d:\d{2}',raw_time).group(0)#first number will be time
-        round_str = re.search(r'\d of \d',raw_time).group(0)#second number will be rounds
-        rounds,_ = round_str.split('of')
-
-    return time,int(rounds)
-
 
 def verifyPrediction(matchups):
     for matchup in matchups:
@@ -300,67 +261,6 @@ def getFightEventResults2(request,eventId):
     
 #maybe asynchronous in the future evaluate
 #fetch results from web if not already in database
-@require_GET
-def getFightEventResults(request,eventId):
-    
-    fightEvent = get_object_or_404(FightEvent,id=eventId)
-    #do not try and get event results if the event is in the future
-    #or if the it is not the atleast 2:00 am the day after the event
-    if fightEvent.date > datetime.date.today() or datetime.datetime.now().hour < 2:
-        return JsonResponse({'fightOutcomes':[],'error':'Results not available yet'})
-    matchups = MatchUp.objects.filter(event=fightEvent)
-    nameMatchupMap = {}
-    
-    fightOutcomes = []
-    hasOutcomes = False
-    for matchup in matchups:
-        nameMatchupMap[matchup.fighter_a.name_unmod] = matchup
-        nameMatchupMap[matchup.fighter_b.name_unmod] = matchup
-        fightOutcome = FightOutcome.objects.filter(matchup=matchup).first()
-        if fightOutcome:
-            fightOutcomes.append(model_to_dict(fightOutcome))
-            hasOutcomes = True
-            
-    if not hasOutcomes:
-        # print(fightEvent.link)
-        outcomes = scraper.getFightEventResults(fightEvent.link)
-        # print(outcomes)
-        for outcome in outcomes:
-            #do what find the corresponding matchup from matchups
-            raw_time = outcome['time']
-            raw_method = outcome['method']
-            fighter_0 = outcome['fighter_0']
-            fighter_1 = outcome['fighter_1']
-
-            if fighter_0 in nameMatchupMap:
-                matchup = nameMatchupMap[fighter_0]
-            if not matchup and not fighter_1 in nameMatchupMap:
-                matchup = nameMatchupMap[fighter_1]
-            if not matchup:#try other fighter name since names can be sometimes different on different sites
-                # matchup = nameMatchupMap[fighter_1]
-                continue#skip result
-            winner = None
-            if 'winner' in outcome:
-                winner_name = outcome[outcome['winner']]
-                #grab the fighter object from the matchup
-                if winner_name == matchup.fighter_a.name_unmod:
-                    winner = matchup.fighter_a
-                else:
-                    winner = matchup.fighter_b
-            method = getFightEndMethod(raw_method)
-            time,final_round = getFightEndTimeAndRound(raw_time)
-            
-            fightOutcome = FightOutcome(
-                matchup=matchup,
-                time=time,
-                method=method,
-                final_round=final_round,
-                winner=winner
-            )
-            fightOutcome.save()
-            fightOutcomes.append(model_to_dict(fightOutcome))
-    # print(fightOutcomes)
-    return JsonResponse({'fightOutcomes':fightOutcomes})
 
 @require_GET
 def event_predictions(request,eventId):
