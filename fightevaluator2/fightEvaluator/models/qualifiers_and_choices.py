77ae4adcaeb95497156b42ae1f3c1781e58acf32
raman-dev <ramandeep.dev582@gmail.com>
2025-02-25 21:03:30 -0500

    changed event value to correct mappings

--- fightevaluator2/fightEvaluator/models/qualifiers_and_choices.py
+++ fightevaluator2/fightEvaluator/models/qualifiers_and_choices.py
@@ -39,13 +39,13 @@ class Likelihood(models.IntegerChoices):
 
 
 class Event(models.TextChoices):
-    WIN = "Win","Fighter wins"  
-    GOES_THE_DISTANCE = "Yes","Fight Goes the Distance"
-    DOES_NOT_GO_THE_DISTANCE = "No","Fight Does Not Go the Distance"
+    WIN = "WIN","Fighter wins"  
+    GOES_THE_DISTANCE = "GOES_THE_DISTANCE","Fight Goes the Distance"
+    DOES_NOT_GO_THE_DISTANCE = "DOES_NOT_GO_THE_DISTANCE","Fight Does Not Go the Distance"
     
-    ROUNDS_GEQ_ZERO_AND_HALF = "Rnds >= 0.5","Fight lasts more than 0.5 rounds"
-    ROUNDS_GEQ_ONE_AND_HALF = "Rnds >= 1.5","Fight lasts more than 1.5 rounds"
-    ROUNDS_GEQ_TWO_AND_HALF = "Rnds >= 2.5","Fight lasts more than 2.5 rounds"
-    ROUNDS_GEQ_THREE_AND_HALF = "Rnds >= 3.5","Fight lasts more than 3.5 rounds"
-    ROUNDS_GEQ_FOUR_AND_HALF = "Rnds >= 4.5","Fight lasts more than 4.5 rounds"
+    ROUNDS_GEQ_ZERO_AND_HALF = "ROUNDS_GEQ_ZERO_AND_HALF","Fight lasts more than 0.5 rounds"
+    ROUNDS_GEQ_ONE_AND_HALF = "ROUNDS_GEQ_ONE_AND_HALF","Fight lasts more than 1.5 rounds"
+    ROUNDS_GEQ_TWO_AND_HALF = "ROUNDS_GEQ_TWO_AND_HALF","Fight lasts more than 2.5 rounds"
+    ROUNDS_GEQ_THREE_AND_HALF = "ROUNDS_GEQ_THREE_AND_HALF","Fight lasts more than 3.5 rounds"
+    ROUNDS_GEQ_FOUR_AND_HALF = "ROUNDS_GEQ_FOUR_AND_HALF","Fight lasts more than 4.5 rounds"
     NA = "NA","Not Available" #unset event
