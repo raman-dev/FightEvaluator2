dad48f5712806c81e1349be9ebbd0b960f65f999
raman-dev <ramandeep.dev582@gmail.com>
2025-04-03 16:04:18 -0400

    meh

--- fightevaluator2/fightEvaluator/vue-profit/src/stores/graphs.js
+++ fightevaluator2/fightEvaluator/vue-profit/src/stores/graphs.js
@@ -29,11 +29,51 @@ export const useGraphStore = defineStore('graph',()=>{
             shape: "box",
             font: {
               bold: {
-                color: "#0077aa",
+                color: "#000000",
               },
             },
           },
     };
+
+    function styleNode(node,type,data){
+        const {likelihood,fighter} = data;
+        let label = '';
+        let color = ''
+        if (type == 'win'){
+            label = `${fighter} wins`;
+        }else if (type=='rounds_geq_one_half') {
+            label = 'Rounds >= 1.5';
+        }else { 
+            label = 'Does Not Go The Distance';
+        }
+
+        switch (likelihood){
+            case 'very likely':
+                color = '#00ff00';
+                break;
+            case 'somewhat likely':
+                color = '#80ff00';
+                break;
+            case 'neutral':
+                color = '#ffc107';
+                break;
+            case 'somewhat unlikely':
+                color = '#ed7b26';
+                break;
+            case 'very unlikely':
+                color = '#dc3545';
+                break;
+            default://not predicted
+                color = '#52565a';
+                break;
+        }
+
+        node.label = '<b>'+ label + '</b>';
+        node.color = color;
+        node.font = {
+            multi:true
+        }
+    }
 
     function toggleGraph(){
 
@@ -59,13 +99,13 @@ export const useGraphStore = defineStore('graph',()=>{
             */
            for (const matchupId in line) {
                 const { eventMap, title } = line[matchupId];
-                for (const type in eventMap) {
-                    if (type == 'count'){
+                for (const eventType in eventMap) {
+                    if (eventType == 'count'){
                         continue;
                     }
-                    const nodeId = `${matchupId}|${type}`;
+                    const nodeId = `${matchupId}|${eventType}`;
                     let node = nodes.get(nodeId);
-                    let nodeLabel = `${title}\n${type}`;
+                    let nodeLabel = `${title}\n${eventType}`;
                     if (node == null || node == undefined){
                         node = {
                             id: nodeId,
@@ -73,6 +113,7 @@ export const useGraphStore = defineStore('graph',()=>{
                         }
                         console.log ('creating node => ',node);
                         nodes.add(node);
+                        styleNode(node,eventType,eventMap[eventType]);
                     }
                     if (prev != null){
                         edges.add({
