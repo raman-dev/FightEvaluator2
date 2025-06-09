15f28f70dc0360af75a6d632115160ce5f5e507a
raman-dev <ramandeep.dev582@gmail.com>
2025-06-09 13:53:28 -0400

    meh

--- fightevaluator2/fightEvaluator/views/index.py
+++ fightevaluator2/fightEvaluator/views/index.py
@@ -209,8 +209,8 @@ def getFightEventResults2(request,eventId):
     #do not try and get event results if the event is in the future
     #or if the it is not the atleast 2:00 am the day after the event
 
-    # if fightEvent.date > datetime.today().date() or datetime.now().hour < 2:
-    #     return JsonResponse({'fightOutcomes':[],'error':'Results not available yet'})
+    if fightEvent.date > datetime.today().date() or datetime.now().hour < 2:
+        return JsonResponse({'fightOutcomes':[],'error':'Results not available yet'})
     matchups = MatchUp.objects.filter(event=fightEvent)
 
     matchupResults = {}
