17af2330f0cc6fbe9b75162d123f6c6c64689549
Ramandeep Bhatti <ramandeep.dev582@gmail.com>
2025-11-27 12:24:12 -0500

    deleted unused files and packages

--- fightevaluator2/form_tester.py
+++
@@ -1,9 +0,0 @@
-from fightEvaluator.models import *
-from fightEvaluator.forms import *
-
-def test():
-    f = Fighter.objects.get(pk = 2166)
-    form  = FighterForm(data = {'data_api_link':'link'},instance=f)
-    print(form.is_valid())
-    print(form.errors)
-test()
