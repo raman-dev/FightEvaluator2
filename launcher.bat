5c37250281de731f320e50b35a0a55164f375977
Ramandeep Bhatti <ramandeep.dev582@gmail.com>
2026-02-19 15:57:53 -0500

    meh

---
+++ launcher.bat
@@ -0,0 +1,6 @@
+rem do what
+rem use to launch my server/service with particular options
+rem example start server --no-scaper
+rem or start scraper
+rem or start scraper --test-client
+rem or start server --port 8080 
